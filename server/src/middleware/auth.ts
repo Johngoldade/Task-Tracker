72b97546fe88a926585df3ed3458abd3f3db09ea
John Goldade (Work) <goldade.john@gmail.com>
2025-03-30 14:09:43 -0500

    Added authentication on sever side

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,17 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authorization: string | undefined = req.headers.authorization
+
+  if (authorization) {
+    const bearerToken: string | undefined = authorization.split(' ').pop()
+    const jwtSecretKey: string | undefined = process.env.JWT_SECRET_KEY
+    jwt.verify(bearerToken as string, jwtSecretKey as string, (error, user) => {
+      error ? res.status(403).send(error) : res.sendStatus(200)
+      req.user = user as JwtPayload
+      return next()
+    })
+  } else {
+    res.sendStatus(401)
+  }
 };
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,23 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const username = req.body.username
+  const password = req.body.password
+
+  const user = await User.findOne({where: {username: username}})
+
+  if(!user) {
+    return res.status(404).send('User not found')
+  } else {
+    const isValidPassword = bcrypt.compare(password, user.password)
+    if (!isValidPassword) {
+      return res.status(401).send('Authentication failed')
+    } else {
+      const jwtSecretKey = process.env.JWT_SECRET_KEY
+      const userToken = jwt.sign(username, jwtSecretKey as string, {expiresIn: '1h'})
+      return res.json({ userToken })
+    }
+  }
 };
 
 const router = Router();
